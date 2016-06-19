f8cd24081cce118965677b957b51b122cef03666
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-19 11:48:17 +0200

    Update description in setup/terminal-theme.sh

--- setup/terminal-theme.sh
+++ setup/terminal-theme.sh
@@ -1,29 +1,23 @@
-## Minimal configuration for Terminal.app
+# # Minimal configuration for Terminal.app
 # 
-# One of the nice things about Terminal.app on OS X is that there's not a lot to
-# configure. The default profile is almost perfect for me. I like using a light
-# background in my terminal, and the colors are nice and clear without any
-# configuration.
+# The default settings in Terminal.app are quite sensible, so there are just three things I set up:
 # 
-# ![Inconsolata in 16pt with a line height of 1.25 in Terminal.app]
-# 
-# Because of these sensible defaults, there are just three things I set up:
-# 
-# * I use [Inconsolata] as my typeface instead of [Menlo].
+# * I use [Inconsolata] as my typeface instead of Menlo.
 # 
 # * For readability, I increased the font size to 16pt and set the line spacing to 1.25.
 # 
 # * I turned off the (visual) bells, badges, and app icon bouncing.
 # 
-# That's all. I exported the [profile] and keep it [in my dotfiles] to make it
+# ![Inconsolata in 16pt with a line height of 1.25 in Terminal.app]
+# 
+# I exported the profile and keep it [in my dotfiles] to make it
 # easy to set up. To use it, [install Inconsolata] and open `Basic+.terminal` in
 # Terminal.app.
-#
+# 
 # [Inconsolata]: https://github.com/google/fonts/tree/master/ofl/inconsolata
-# [Menlo]: https://en.wikipedia.org/wiki/Menlo_(typeface)
 # [in my dotfiles]: https://github.com/jeffkreeftmeijer/dotfiles/blob/master/Basic%2B.terminal
 # [Inconsolata in 16pt with a line height of 1.25 in Terminal.app]: images/basic+.png
-# [Install Inconsolata]: https://github.com/jeffkreeftmeijer/dotfiles/blob/master/setup/inconsolata.sh
+# [Install Inconsolata]: https://github.com/jeffkreeftmeijer/dotfiles/blob/master/setup/brew/inconsolata.sh
 
 osascript <<EOD
 tell application "Terminal"
